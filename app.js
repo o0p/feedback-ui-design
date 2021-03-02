fc0d6f15f2a26eba982fc075165eb15b383afc63
Russell <russelldev@protonmail.com>
2021-03-02 23:49:06 +0000

    Add panel listener for ratings

--- app.js
+++ app.js
@@ -2,3 +2,8 @@ const ratings = document.querySelectorAll('.rating');
 const sendBtn = document.querySelector('#send');
 const panel = document.querySelector('#panel');
 
+panel.addEventListener('click', (e) => {
+    if (e.target.parentNode.classList.contains('rating')) {
+        e.target.parentNode.classList.add('active');
+    };
+});
