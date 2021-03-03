9f5b99f27cf7380892a2f72ed26c70c80484801b
Russell <russelldev@protonmail.com>
2021-03-03 12:00:05 +0000

    Add selectedRating var

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const ratings = document.querySelectorAll('.rating');
 const sendBtn = document.querySelector('#send');
 const panel = document.querySelector('#panel');
+let selectedRating = 'Satified';
 
 panel.addEventListener('click', (e) => {
     if (e.target.parentNode.classList.contains('rating')) {
